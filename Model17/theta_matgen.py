3d9b2a61d9b09fb7c35ffe7c936243aaf86a68e9
Mgha2511 <112585753+Mgha2511@users.noreply.github.com>
2022-09-15 08:55:06 +0200

    Add files via upload

---
+++ Model17/theta_matgen.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 29 19:32:17 2022
+
+@author: meghar
+"""
+import numpy as np
+from scipy.io import savemat
+
+inp = 1
+hl_1 = 4
+out = 3
+
+t1 = np.random.rand(hl_1, inp+1)
+t2 = np.random.rand(out, hl_1+1)
+
+savemat('theta.mat', mdict={'Theta1': t1, 'Theta2': t2})
